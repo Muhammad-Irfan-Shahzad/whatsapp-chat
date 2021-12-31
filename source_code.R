9d5e0cd0b9c85ab79d822c6a048228a022dd94e4
M.Irfan Shahzad <43313225+Muhammad-Irfan-Shahzad@users.noreply.github.com>
2021-12-31 11:55:34 +0500

    Create source_code.R

---
+++ source_code.R
@@ -0,0 +1,72 @@
+library(stringr)
+library(ggplot2)
+library("rwhatsapp")
+library("dplyr")
+## 
+## Attaching package: 'dplyr'
+## The following objects are masked from 'package:stats':
+## 
+##     filter, lag
+## The following objects are masked from 'package:base':
+## 
+##     intersect, setdiff, setequal, union
+library("lubridate")
+## 
+## Attaching package: 'lubridate'
+## The following objects are masked from 'package:base':
+## 
+##     date, intersect, setdiff, union
+library("tidyr")
+library("tidytext")
+library("stopwords")
+
+chat <- rwa_read("chat.txt")
+chat <- rwa_read("chat.txt") %>% 
+  filter(!is.na(author)) 
+
+chat %>%
+  mutate(day = date(time)) %>%
+  count(day) %>%
+  ggplot(aes(x = day, y = n)) +
+  geom_bar(stat = "identity") +
+  ylab("") + xlab("") +
+  ggtitle("Messages per day")
+
+
+
+
+
+You can also embed plots, for example:
+chat %>%
+  unnest(emoji) %>%
+  count(author, emoji, sort = TRUE) %>%
+  group_by(author) %>%
+  top_n(n = 6, n) %>%
+  ggplot(aes(x = reorder(emoji, n), y = n, fill = author)) +
+  geom_col(show.legend = FALSE) +
+  ylab("") +
+  xlab("") +
+  coord_flip() +
+  facet_wrap(~author, ncol = 2, scales = "free_y")  +
+  ggtitle("Most often used emojis")
+
+
+
+
+
+
+chat %>%
+  unnest_tokens(input = text,
+                output = word) %>%
+  count(author, word, sort = TRUE) %>%
+  group_by(author) %>%
+  top_n(n = 4, n) %>%
+  ggplot(aes(x = reorder_within(word, n, author), y = n, fill = author)) +
+  geom_col(show.legend = FALSE) +
+  ylab("") +
+  xlab("") +
+  coord_flip() +
+  facet_wrap(~author, ncol = 2, scales = "free_y") +
+  scale_x_reordered() +
+  ggtitle("Most often used words")
+
